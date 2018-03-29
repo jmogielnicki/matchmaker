9ea5a5fcac9c5d81b5513c6b41163581c93f69f5
John Mogielnicki <jmogielnicki@pinterest.com>
2018-03-28 18:14:36 -0700

    output matches to sheets

--- matchmaker.py
+++ matchmaker.py
@@ -7,6 +7,7 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from pprint import pprint
 
 try:
     import argparse
@@ -16,10 +17,14 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 
+INPUT_RANGE_NAME = 'office_list!A2:C400'
+OPT_OUT_RANGE_NAME = 'Form Responses 2!A1:C100'
+OUTPUT_RANGE_NAME = 'matchmaker_output!A1:W400'
+SPREADSHEET_ID = '1mb2_RuTwYXChc9wyebzVXJ75uh7mgqof220eZvFc9Iw'
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -49,19 +54,91 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def matchmaker(values):
+def get_people_list(service, spreadsheetId, rangeName):
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        return values
+
+def match_make(values):
+    print('length of values:')
+    print(len(values))
     current_group = []
     groups = []
     group_size = 2
     people = [row for row in values]
+    print('length of people:')
+    print(len(people))
     random.shuffle(people)
 
     groups = [people[i:i + group_size] for i in range(0, len(people), group_size)]
+    print(groups)
+
     # If the last group is too small, add to last group
     if len(groups[-1]) < 2:
         leftovers = groups.pop()
         groups[-1] = groups[-1] + leftovers
+        print(groups[-1])
+    print('length of groups:')
+    print(len(groups))
     return groups
+
+def write_groups_to_sheets(groups):
+
+    credentials = get_credentials()
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+
+    # The ID of the spreadsheet to update.
+    spreadsheet_id = SPREADSHEET_ID
+
+    # The A1 notation of the values to update.
+    range_ = OUTPUT_RANGE_NAME  # TODO: Update placeholder value.
+
+    # How the input data should be interpreted.
+    value_input_option = 'RAW'  # TODO: Update placeholder value.
+
+    value_range_body = {
+        "range": OUTPUT_RANGE_NAME,
+        "majorDimension": "ROWS",
+        "values": groups
+    }
+
+    request = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id,
+        range=range_,
+        valueInputOption=value_input_option,
+        body=value_range_body
+    )
+    response = request.execute()
+    pprint(response)
+    # service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
+
+def modify_data(matches):
+    headers = [
+        'person 1 name',
+        'person 1 ldap',
+        'person 1 team',
+        'person 2 name',
+        'person 2 ldap',
+        'person 2 team',
+        'person 3 name',
+        'person 3 ldap',
+        'person 3 team'
+        ]
+    modified_matches = [match[0] + match[1] for match in matches]
+    modified_matches = []
+    for match in matches:
+        match_list = []
+        for item in match:
+            match_list = match_list + item
+        modified_matches.append(match_list)
+    import pdb; pdb.set_trace()
+    modified_matches.insert(0, headers)
+    return modified_matches
 
 def main():
     """Shows basic usage of the Sheets API.
@@ -77,21 +154,18 @@ def main():
     service = discovery.build('sheets', 'v4', http=http,
                               discoveryServiceUrl=discoveryUrl)
 
-    spreadsheetId = '1mb2_RuTwYXChc9wyebzVXJ75uh7mgqof220eZvFc9Iw'
-    rangeName = 'List!A2:B400'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
+    spreadsheetId = SPREADSHEET_ID
+    rangeName = INPUT_RANGE_NAME
+    values = get_people_list(service, spreadsheetId, rangeName)
 
-    if not values:
-        print('No data found.')
-    else:
-        matches = matchmaker(values)
-        print(matches)
-        for match in matches:
-            print(match)
-            # print(', '.join(match[1]))
+    matches = match_make(values)
+    # print(matches)
+    matches = modify_data(matches)
+    # print(matches)
+    for match in matches:
+        print(match)
 
+    write_groups_to_sheets(matches)
 
 if __name__ == '__main__':
     main()
